c482038152b4b4801ba44c74c563ca81f043cdd2
Kazerlelutin <b@bouteiller.contact>
2025-11-22 17:56:15 +0100

    add children

--- features/members/members.GET.js
+++ features/members/members.GET.js
@@ -3,7 +3,6 @@ const { knex } = require("../../db/db");
 const activeStatuts = [1, 2, 3, 4];// President, Treasurer, Secrétaire, Membre actif
 module.exports = async (r, h) => {
 
-
   const { email } = r.query;
   try {
     if (email) {
@@ -13,6 +12,21 @@ module.exports = async (r, h) => {
         .where('galette_adherents.email_adh', email)
         .select('galette_adherents.*', 'galette_statuts.libelle_statut as statut_nom')
         .first();
+
+      if (member) {
+        delete member.mdp_adh;
+      }
+
+      const children = await knex('galette_adherents')
+        .where('galette_adherents.parent_id', member.id_adh)
+        .select('galette_adherents.*')
+        .first();
+
+      if (children) {
+        delete children.mdp_adh;
+      }
+
+      member.children = children;
       return h.response(member);
     }
     const members = await knex('galette_adherents')
@@ -24,6 +38,11 @@ module.exports = async (r, h) => {
       })
       .select('galette_adherents.*', 'galette_statuts.libelle_statut as statut_nom')
       .orderBy('galette_adherents.date_echeance', 'asc');
+
+    members.forEach(member => {
+      delete member.mdp_adh;
+    });
+
     return h.response(members);
   } catch (error) {
     return h.response({ error }).code(500);
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require('./features/routes/routes');
 const init = async () => {
 
   const server = Hapi.server({
-    port: 3000,
+    port: 4500,
     host: 'localhost'
   });
 
