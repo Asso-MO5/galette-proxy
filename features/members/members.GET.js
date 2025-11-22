d8d18f76a3710b3b09810c7a8a8a47cff1609fb9
Kazerlelutin <b@bouteiller.contact>
2025-11-22 15:21:42 +0100

    add search

--- features/members/members.GET.js
+++ features/members/members.GET.js
@@ -1,17 +1,29 @@
 const { knex } = require("../../db/db");
 
+const activeStatuts = [1, 2, 3, 4];// President, Treasurer, Secrétaire, Membre actif
 module.exports = async (r, h) => {
 
 
+  const { email } = r.query;
   try {
+    if (email) {
+      const member = await knex('galette_adherents')
+        .join('galette_statuts', 'galette_adherents.id_statut', 'galette_statuts.id_statut')
+        .whereIn('galette_adherents.id_statut', activeStatuts)
+        .where('galette_adherents.email', email)
+        .select('galette_adherents.*', 'galette_statuts.libelle_statut as statut_nom')
+        .first();
+      return h.response(member);
+    }
     const members = await knex('galette_adherents')
       .join('galette_statuts', 'galette_adherents.id_statut', 'galette_statuts.id_statut')
-      .whereIn('galette_adherents.id_statut', [1, 2, 3, 4]) // President, Treasurer, Secrétaire, Membre actif
+      .whereIn('galette_adherents.id_statut', activeStatuts)
       .where(function () {
         this.where('galette_adherents.date_echeance', '>', new Date())
           .orWhere('galette_adherents.bool_exempt_adh', true);
       })
-      .select('galette_adherents.*', 'galette_statuts.libelle_statut as statut_nom');
+      .select('galette_adherents.*', 'galette_statuts.libelle_statut as statut_nom')
+      .orderBy('galette_adherents.date_echeance', 'asc');
 
     console.log(members.length);
     return h.response(members);
